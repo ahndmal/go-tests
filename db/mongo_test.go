beeb1277909ccee6cec7c0ced62765caf0c48edb
andrii <quadr988@gmail.com>
2023-11-08 15:54:21 +0200

    ctx

---
+++ db/mongo_test.go
@@ -0,0 +1,72 @@
+package db
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"os"
+	"testing"
+	"time"
+)
+
+type Workout struct {
+	Id           string    `json:"_id" bson:"_id,omitempty"`
+	Record       int64     `json:"record"`
+	Sets         int       `json:"sets"`
+	Comments     string    `json:"comments"`
+	CreationDate time.Time `json:"creation_date" bson:"creation_date"`
+	WorkoutDate  string    `json:"workout_date" bson:"workout_date"`
+	Day          string    `json:"day"`
+	Week         int       `json:"week"`
+	WorkoutType  string    `json:"workout_type" bson:"workout_type"`
+	Month        string    `json:"month"`
+	Year         int       `json:"year"`
+}
+
+type WorkoutDto struct {
+	Id           string `json:"_id" bson:"_id,omitempty"`
+	Record       int64  `json:"record"`
+	Sets         int    `json:"sets"`
+	Comments     string `json:"comments"`
+	CreationDate string `json:"creation_date" bson:"creation_date"`
+	WorkoutDate  string `json:"workout_date" bson:"workout_date"`
+	Day          string `json:"day"`
+	Week         int    `json:"week"`
+	WorkoutType  string `json:"workout_type" bson:"workout_type"`
+	Month        string `json:"month"`
+	Year         int    `json:"year"`
+}
+
+func TestGetWorkouts(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	mongoUri := os.Getenv("MONGO_URL")
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoUri))
+	if err != nil {
+		log.Printf("> error connecting to MongoDB: %v", err)
+	}
+
+	collection := client.Database("workouts").Collection("workouts")
+
+	filter := bson.D{{"workout_date", "2023-11-05"}}
+
+	curr, err := collection.Find(context.Background(), filter)
+	defer curr.Close(ctx)
+
+	if err != nil {
+		log.Printf("> error dinding data in MongoDB: %v", err)
+	}
+
+	workouts := make([]Workout, 0)
+
+	curr.All(context.Background(), &workouts)
+
+	for _, w := range workouts {
+		log.Println(w)
+	}
+
+}
--- go.mod
+++ go.mod
@@ -29,6 +29,7 @@ require (
 	github.com/konsorten/go-windows-terminal-sequences v1.0.3 // indirect
 	github.com/linkedin/goavro/v2 v2.12.0 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
 	github.com/mtibben/percent v0.2.1 // indirect
 	github.com/pierrec/lz4 v2.6.1+incompatible // indirect
 	github.com/pkg/errors v0.9.1 // indirect
@@ -38,11 +39,18 @@ require (
 	github.com/prometheus/procfs v0.9.0 // indirect
 	github.com/sirupsen/logrus v1.9.0 // indirect
 	github.com/spaolacci/murmur3 v1.1.0 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.2 // indirect
+	github.com/xdg-go/stringprep v1.0.4 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	go.mongodb.org/mongo-driver v1.12.1 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	go.uber.org/atomic v1.10.0 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
 	golang.org/x/mod v0.10.0 // indirect
 	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/oauth2 v0.8.0 // indirect
+	golang.org/x/sync v0.2.0 // indirect
 	golang.org/x/sys v0.13.0 // indirect
 	golang.org/x/term v0.13.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
